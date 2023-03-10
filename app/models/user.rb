4d96ee3b7cf722f956e3b309175c742b8984e76b
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-03-10 16:49:51 +0300

    fix typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
     before_save {self.email = email.downcase}
+    validates :user_name, presence: true, length: { maximum: 25 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, length: {minimum:3, maximum:50},
-                uniqueness: {case_sensitive: false}, 
-                format: { with: VALID_EMAIL_REGEX}
-    validate :user_name, presence:true
-    validates :password_digest, presennce: true
+    validates :email,
+    presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false },
+    format: { with: VALID_EMAIL_REGEX}
+    
+    validates :password_digest, presence: true
     has_secure_password 
 end
