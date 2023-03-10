79b03168412fd4d9317d763c406ba2f5112c4995
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-03-10 16:27:32 +0300

    add user configs to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
-    
+    before_save {self.email = email.downcase}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: {minimum:3, maximum:50},
+                uniqueness: {case_sensitive: false}, 
+                format: { with: VALID_EMAIL_REGEX}
+    validate :user_name, presence:true
+    validates :password_digest, presennce: true
+    has_secure_password 
 end
