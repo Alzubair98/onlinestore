f3c95d851875cb70680a3c3853b50311e18ddcb9
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-03-14 22:42:22 +0300

    add log in function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,11 @@ class SessionsController < ApplicationController
     end
 
     def create 
-        user = User.find_by(email: params[:session][:eamil].downcase)
+        user = User.find_by(email: params[:session][:email].downcase)
         if user && user.authenticate(params[:session][:password])
             session[:user_id] = user.id
+            redirect_to root_path
+            
         end
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete :logout, to: 'sessions#destroy'
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
 end
